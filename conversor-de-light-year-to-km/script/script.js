59ac63b9a9cefc96535fc7145e48300600560bff
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-03-22 21:43:26 -0300

    Primeiro commit

---
+++ conversor-de-light-year-to-km/script/script.js
@@ -0,0 +1,7 @@
+﻿function converterEmKm() {
+    var valueInputLy = document.getElementById("inputvaluely").value;
+    var valueEmKm = (parseFloat(valueInputLy) * 9460528404879.4).toExponential(4);
+
+    document.getElementById("valuekmconverted").innerHTML = " " +
+        valueEmKm + " Km";
+}
---
+++ conversor-de-moedas/script/script.js
@@ -0,0 +1,9 @@
+﻿    // var valorEmDolar = parseFloat(prompt("Qual é o valor em Dolars U$ da sua viagem?"));
+
+    function converter() {
+        var valueDolar = document.getElementById("valueinput").value
+        var valueReal = (parseFloat(valueDolar) * 5.50).toFixed(2)
+
+        document.getElementById("valueconverted").innerHTML = "R$" + (valueReal).replace(".", ",");
+
+    }

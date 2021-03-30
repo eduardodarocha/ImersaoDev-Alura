09881e9210c7a1351e07b66cbab6835baf85cb44
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-03-30 00:45:09 -0300

    Primeiro commit da tabela de classificação v1.0

---
+++ tabela-de-classificacao/script/script.js
@@ -0,0 +1,79 @@
+﻿var paulo = {
+    nome: "Paulo",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+};
+
+var rafa = {
+    nome: "Rafa",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+};
+
+function calculaPontos(jogador) {
+    var pontos = jogador.vitorias * 3 + jogador.empates;
+    return pontos;
+}
+
+rafa.pontos = calculaPontos(rafa);
+paulo.pontos = calculaPontos(paulo);
+
+var jogadores = [rafa, paulo];
+
+function exibirJogadoresNaTela(jogadores) {
+    var html = "";
+    for (var i = 0; i < jogadores.length; i++) {
+        html += "<tr><td>" + jogadores[i].nome + "</td>";
+        html += "<td>" + jogadores[i].vitorias + "</td>";
+        html += "<td>" + jogadores[i].empates + "</td>";
+        html += "<td>" + jogadores[i].derrotas + "</td>";
+        html += "<td>" + jogadores[i].pontos + "</td>";
+        html +=
+            "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+
+        if (i == 0) {
+            html += '<th rowspan="2"><button onClick="jogadores.forEach(adicionarEmpate)">Empate</button></th>';
+            html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></th></tr>";
+        } else {
+
+            html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></tr>";
+        }
+    }
+    var tabelaJogadores = document.getElementById("tabelaJogadores");
+    tabelaJogadores.innerHTML = html;
+}
+
+exibirJogadoresNaTela(jogadores);
+
+function adicionarVitoria(i) {
+    var jogador = jogadores[i];
+    jogador.vitorias++;
+    jogador.pontos = calculaPontos(jogador);
+    exibirJogadoresNaTela(jogadores);
+}
+
+function adicionarEmpate(jogador) {
+    jogador.empates++;
+    jogador.pontos = calculaPontos(jogador);
+    exibirJogadoresNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+    var jogador = jogadores[i];
+    jogador.derrotas++;
+    exibirJogadoresNaTela(jogadores);
+}
+
+function limpar() {
+    for (var i = 0; i < jogadores.length; i++) {
+        jogadores[i].vitorias = 0
+        jogadores[i].derrotas = 0
+        jogadores[i].empates = 0
+        jogadores[i].pontos = 0
+    }
+    exibirJogadoresNaTela(jogadores)
+}

75b0560a7c8214377088a232717c4ef61a12e3a6
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-04-05 01:10:02 -0300

    Elaboração do Supertrunfo v2

---
+++ super-trunfo_v2/script/script.js
@@ -0,0 +1,339 @@
+﻿var shuraDeCapricornio = {
+    nome: "Shura de Capricórnio",
+    imagem: "https://3.bp.blogspot.com/-d585GGRK0MU/Wx6I1kj0PEI/AAAAAAAAaSw/Fgtoo6LCnuIUHG36LwgHIwEfe_XRkP1JwCLcBGAs/s1600/20881924_1530734130298467_3074400605477614367_n.jpg",
+    atributos: {
+        forçaFisica: 9.0,
+        agilidade: 9.5,
+        ataqueADistancia: 8.5,
+        resistencia: 9.5,
+        inteligenciaETatica: 7.5,
+        letalidade: 8.5,
+        cosmo: 8.5
+    }
+};
+var afroditeDePeixes = {
+    nome: "Afrodite De Peixes",
+    imagem: "https://3.bp.blogspot.com/-t-bZ8dw6BzQ/Wx6I1OdNH6I/AAAAAAAAaSo/LlrOENyjDBsFzuKauaGcRcXt4tZMhj8TACLcBGAs/s1600/20914171_1530734333631780_2557915094780313145_n.jpg",
+    atributos: {
+        forçaFisica: 7.5,
+        agilidade: 8.0,
+        ataqueADistancia: 8.5,
+        resistencia: 7.0,
+        inteligenciaETatica: 9.0,
+        letalidade: 9.5,
+        cosmo: 7.5
+    }
+};
+var aldebaranDeTouro = {
+    nome: "Aldebaran De Touro",
+    imagem: "https://3.bp.blogspot.com/-RG9rx7Cjn0I/Wx6I1n34beI/AAAAAAAAaSs/10wj6DVLsnQ4GFOqnJ6s9JZ6elCj41dHACLcBGAs/s1600/20914335_1530733140298566_524967136346256837_n.jpg",
+    atributos: {
+        forçaFisica: 9.5,
+        agilidade: 8.0,
+        ataqueADistancia: 7.5,
+        resistencia: 8.0,
+        inteligenciaETatica: 7.5,
+        letalidade: 7.0,
+        cosmo: 8.0
+    }
+};
+var camusDeAquario = {
+    nome: "Camus de Aquário",
+    imagem: "https://3.bp.blogspot.com/--vAw6GzqxNg/Wx6I16NmhoI/AAAAAAAAaS0/Y5rPrV4f-AkclO-5jYZQbqX2OVLEyuQUACLcBGAs/s640/20915106_1530734260298454_3487529058948041195_n.jpg",
+    atributos: {
+        forçaFisica: 8.0,
+        agilidade: 8.0,
+        ataqueADistancia: 8.5,
+        resistencia: 9.5,
+        inteligenciaETatica: 9.0,
+        letalidade: 8.0,
+        cosmo: 8.5
+    }
+};
+var miloDeEscorpiao = {
+    nome: "Milo de Escorpiao",
+    imagem: "https://3.bp.blogspot.com/-lj0-FRWR3DE/Wx6I2SD6bgI/AAAAAAAAaS4/1r4OJnCSfI8_Qbax05KFHy1FuEFU_0VVQCLcBGAs/s1600/20915159_1530734000298480_8983170410146718855_n.jpg",
+    atributos: {
+        forçaFisica: 8.5,
+        agilidade: 10.0,
+        ataqueADistancia: 8.5,
+        resistencia: 8.5,
+        inteligenciaETatica: 8.0,
+        letalidade: 9.0,
+        cosmo: 8.0
+    }
+};
+var dohkoDeLibra = {
+    nome: "Dohko de Libra",
+    imagem: "https://3.bp.blogspot.com/-VAu2-tq8jjI/Wx6I21HQSeI/AAAAAAAAaS8/yhgRzRjOpPI7m0nRFkAnNRCkHwrLVqD6ACLcBGAs/s1600/20915488_1530733963631817_4530472654603015018_n.jpg",
+    atributos: {
+        forçaFisica: 8.5,
+        agilidade: 8.5,
+        ataqueADistancia: 9.0,
+        resistencia: 10.0,
+        inteligenciaETatica: 10.0,
+        letalidade: 7.5,
+        cosmo: 9.0
+    }
+};
+var shakaDeVirgem = {
+    nome: "Shaka de Virgem",
+    imagem: "https://2.bp.blogspot.com/-Ud9ywvSHN3E/Wx6I3SKqjFI/AAAAAAAAaTA/VaJ_GwyjhNMgyFI1KJYi0zDzKRzfG_lzgCLcBGAs/s1600/20915591_1530733893631824_8044930329133651315_n.jpg",
+    atributos: {
+        forçaFisica: 7.5,
+        agilidade: 7.5,
+        ataqueADistancia: 9.0,
+        resistencia: 8.0,
+        inteligenciaETatica: 9.5,
+        letalidade: 8.0,
+        cosmo: 9.5
+    }
+};
+var sagaDeGemeos = {
+    nome: "Saga de Gmeos",
+    imagem: "https://4.bp.blogspot.com/--lSKIGaxyjY/Wx6I3ioqLfI/AAAAAAAAaTE/ZzmmHQ9dk5863CF59hIMoZAgPL-rl5iBQCLcBGAs/s1600/20953195_1530733320298548_4127097003252279406_n.jpg",
+    atributos: {
+        forçaFisica: 9.0,
+        agilidade: 7.5,
+        ataqueADistancia: 9.5,
+        resistencia: 10.0,
+        inteligenciaETatica: 8.5,
+        letalidade: 8.0,
+        cosmo: 9.5
+    }
+};
+var muDeAries = {
+    nome: "Mu de Áries",
+    imagem: "https://4.bp.blogspot.com/-p9MgjO0U_vk/Wx6I32YN2eI/AAAAAAAAaTI/kZ_wZiFu-Y8auxbNaV6f6TgCF_sLcFGWgCLcBGAs/s1600/20953255_1530732723631941_2194201203178064655_n.jpg",
+    atributos: {
+        forçaFisica: 7.5,
+        agilidade: 7.5,
+        ataqueADistancia: 8.5,
+        resistencia: 7.0,
+        inteligenciaETatica: 9.5,
+        letalidade: 8.0,
+        cosmo: 8.5
+    }
+};
+var aioliaDeLeao = {
+    nome: "Aiolia de Leão",
+    imagem: "https://3.bp.blogspot.com/-SuEgUPSLuws/Wx6I4C_febI/AAAAAAAAaTM/GmoxCP6cINMZ0OBLUqUkHhtYkSanDVSDgCLcBGAs/s1600/20953375_1530733650298515_3161926766611479636_n.jpg",
+    atributos: {
+        forçaFisica: 9.0,
+        agilidade: 9.0,
+        ataqueADistancia: 8.5,
+        resistencia: 9.0,
+        inteligenciaETatica: 7.5,
+        letalidade: 7.5,
+        cosmo: 8.0
+    }
+};
+var mascaraDaMorteDeCancer = {
+    nome: "Mascara da Morte de Câncer ",
+    imagem: "https://1.bp.blogspot.com/-dGNvARF3D0c/Wx6I4fDBPdI/AAAAAAAAaTQ/qvfE0DyWXwUadSPmSy5UGbuyNTiAG85yACLcBGAs/s1600/20953497_1530733506965196_2223498397055721831_n.jpg",
+    atributos: {
+        forçaFisica: 7.5,
+        agilidade: 7.5,
+        ataqueADistancia: 8.0,
+        resistencia: 7.0,
+        inteligenciaETatica: 8.0,
+        letalidade: 9.0,
+        cosmo: 7.5
+    }
+};
+var aiolosDeSargitario = {
+    nome: "Aiolos de Sargitário",
+    imagem: "https://2.bp.blogspot.com/-IEq1zZ8H2Q4/Wx6I4m6OYoI/AAAAAAAAaTU/lFgmesFWnW0arhPfuwNuIFtAaAug7022gCLcBGAs/s1600/20992581_1530734106965136_7838031216265773046_n.jpg",
+    atributos: {
+        forçaFisica: 9.0,
+        agilidade: 9.0,
+        ataqueADistancia: 8.5,
+        resistencia: 9.0,
+        inteligenciaETatica: 8.0,
+        letalidade: 7.5,
+        cosmo: 8.5
+    }
+};
+
+var nomeAtributo = {
+    0: "Força Física",
+    1: "Agilidade",
+    2: "Ataque a Distância",
+    3: "Resistência",
+    4: "Inteligência e Tática",
+    5: "Letalidade",
+    6: "Cosmo"
+}
+
+
+var cartaMaquina;
+var cartaJogador;
+var cartas = [shuraDeCapricornio, afroditeDePeixes, aldebaranDeTouro, camusDeAquario, miloDeEscorpiao, dohkoDeLibra, shakaDeVirgem, sagaDeGemeos, muDeAries, aioliaDeLeao, mascaraDaMorteDeCancer, aiolosDeSargitario];
+var pontosJogador = 0;
+var pontosMaquina = 0;
+
+atualizaPlacar();
+atualizaQuantidadeDeCartas()
+
+function inicio() {
+    document.getElementById("imagens").innerHTML = "<img id='carta' src='https://i.pinimg.com/736x/5e/e1/5e/5ee15e6e5a543357dd0542dbfc50da1f.jpg'>"
+    document.getElementById("imagensx").innerHTML = "<img id='imagex' src='https://imagensemoldes.com.br/wp-content/uploads/2021/02/Clip-Art-Letra-X-PNG.png'>"
+
+    document.getElementById("imagens2").innerHTML = "<img id='carta' src='https://i.pinimg.com/736x/5e/e1/5e/5ee15e6e5a543357dd0542dbfc50da1f.jpg'>"
+        // var botaoSortear = document.getElementById("btnSortear");
+        // botaoSortear.style.display = "block"
+    var botaoJogar = document.getElementById("btnJogar");
+    botaoJogar.style.display = "none"
+    var opcoes = document.getElementById("opcoes");
+    opcoes.style.display = "none";
+    var mostraresultado = document.getElementById("resultado");
+    mostraresultado.style.display = "none";
+
+}
+inicio()
+
+function atualizaPlacar() {
+    var divPlacar = document.getElementById("placar");
+    var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Máquina";
+
+    divPlacar.innerHTML = html;
+}
+
+function atualizaQuantidadeDeCartas() {
+    var divQuantidadeCartas = document.getElementById("quantidade-cartas");
+    var html = "Quantidade de cartas no jogo: " + cartas.length;
+
+    divQuantidadeCartas.innerHTML = html;
+}
+
+// function mostrarimagens(indiceplayer, indicemachine) {
+function mostrarimagens() {
+    // cartaMachineShow = document.getElementById("imagens2")
+    document.getElementById("imagens").innerHTML = `<img id='carta' src='${cartaJogador.imagem}'>`
+
+    // document.getElementById("imagensx").innerHTML = "<img id='imagex' src='https://imagensemoldes.com.br/wp-content/uploads/2021/02/Clip-Art-Letra-X-PNG.png'>"
+
+    document.getElementById("imagens2").innerHTML = "<img id='carta' src='https://i.pinimg.com/736x/5e/e1/5e/5ee15e6e5a543357dd0542dbfc50da1f.jpg'>"
+    document.getElementById("btnSortear").disabled = false;
+
+}
+
+var numeroCartaMaquina = ""
+
+function sortearCarta() {
+    numeroCartaMaquina = parseInt(Math.random() * cartas.length);
+    console.log("numeroCartaMaquina " + numeroCartaMaquina)
+
+    cartaMaquina = cartas[numeroCartaMaquina];
+    console.log("cartaMaquina " + cartaMaquina)
+
+    cartas.splice(numeroCartaMaquina, 1);
+    console.log(cartas)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length);
+    console.log("numeroCartaJogador " + numeroCartaJogador)
+        //document.getElementById("imagens").innerHTML = "<img id='carta' src='" + cartas[numeroCartaJogador].imagem + "'>"
+    cartaJogador = cartas[numeroCartaJogador];
+    console.log("cartaJogador " + cartaJogador)
+    cartas.splice(numeroCartaJogador, 1);
+    console.log(cartas)
+
+    // while (numeroCartaJogador == numeroCartaMaquina) {
+    //     numeroCartaJogador = parseInt(Math.random() * 13);
+    // }
+    // mostrarimagens(numeroCartaJogador, numeroCartaMaquina)
+    mostrarimagens();
+    // document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").style.display = "inline";
+    document.getElementById("escolheatributotexto").style.display = "block";
+    //document.getElementById("btnJogar").disabled = false;
+    var escondeForm = document.getElementById("form");
+    escondeForm.style.display = "block";
+    var mostraresultado = document.getElementById("resultado");
+    mostraresultado.style.display = "none";
+
+    exibirOpcoes();
+    atualizaQuantidadeDeCartas()
+}
+
+function exibirOpcoes() {
+    var opcoes = document.getElementById("opcoes");
+    opcoes.style.display = "block";
+
+    var opcoesTexto = "";
+    var contAtributo = 0;
+
+    for (var atributo in cartaJogador.atributos) {
+        // opcoesTexto +=
+        //     "<input type='radio' name='atributo' value='" +
+        //     atributo + "'>" + nomeAtributo[contAtributo]
+        opcoesTexto += `<input type='radio' name='atributo' value='${atributo}'>` + nomeAtributo[contAtributo]
+
+        contAtributo++
+    }
+
+    opcoes.innerHTML = opcoesTexto;
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName("atributo");
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value;
+        }
+    }
+}
+
+function jogar() {
+    var atributoSelecionado = obtemAtributoSelecionado();
+
+    var mostraresultado = document.getElementById("resultado");
+    mostraresultado.style.display = "block";
+    console.log(cartaJogador.atributos[atributoSelecionado]);
+    console.log(cartaMaquina.atributos[atributoSelecionado]);
+    if (
+        cartaJogador.atributos[atributoSelecionado] >
+        cartaMaquina.atributos[atributoSelecionado]
+    ) {
+        // alert("Venceu a carta do Jogador");
+        mostraresultado.innerHTML = "A sua carta venceu!";
+        pontosJogador++;
+    } else if (
+        cartaJogador.atributos[atributoSelecionado] <
+        cartaMaquina.atributos[atributoSelecionado]
+    ) {
+        //alert("Venceu a carta do Maquina");
+        mostraresultado.innerHTML = "Sua carta perdeu!";
+        pontosMaquina++
+    } else {
+        // alert("Empate");
+        mostraresultado.innerHTML = "Empate!"
+    }
+    var imagem2 = document.getElementById("imagens2")
+    imagem2.innerHTML = `<img id='carta' src='${cartaMaquina.imagem}'>`;
+    //"<img id='carta' src='" + cartas[numeroCartaMaquina].imagem + "'>";
+    var atributos = document.getElementById("opcoes");
+    atributos.style.display = "block";
+    // var botaoSortear = document.getElementById("btnSortear");
+    // botaoSortear.style.display = "none"
+    var escondeForm = document.getElementById("form");
+    escondeForm.style.display = "none";
+    if (cartas.length == 0) {
+        if (pontosJogador > pontosMaquina) {
+            mostraresultado.innerHTML = "Você venceu a partida, parabéns!";
+            //'<p class="resultado-final">Venceu</p>';
+        } else if (pontosMaquina > pontosJogador) {
+            mostraresultado.innerHTML = "A máquina venceu a partida!"
+                //'<p class="resultado-final">Perdeu</p>';
+        } else {
+            mostraresultado.innerHTML = "Você empatou com a máquina!"
+                //'<p class="resultado-final">Empatou</p>';
+        }
+    }
+    atualizaPlacar();
+
+    //inicio();
+    //sortearCarta()
+
+
+}

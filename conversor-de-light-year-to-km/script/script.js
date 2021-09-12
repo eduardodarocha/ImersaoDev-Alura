3393deaf9932b57bc67d8901e765d4ac50b80aa2
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-09-12 17:40:17 -0300

    Update converter ly to km - link and input

--- conversor-de-light-year-to-km/script/script.js
+++ conversor-de-light-year-to-km/script/script.js
@@ -1,7 +1,11 @@
 ﻿function converterEmKm() {
     var valueInputLy = document.getElementById("inputvaluely").value;
+    if (valueInputLy == "") {
+        alert("Digite um valor");
+    } else{
     var valueEmKm = (parseFloat(valueInputLy) * 9460528404879.4).toExponential(4);
 
     document.getElementById("valuekmconverted").innerHTML = " " +
         valueEmKm + " Km";
+    }
 }
